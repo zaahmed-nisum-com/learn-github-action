3b4c0b58454e389eec4a4ba743a2b18c59c158fa
Zain Ahmed <zaahmed@nisum.com>
2023-02-10 18:13:24 +0500

    docker compose github action

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const getFakeProducts = () => {
-    try {
-        const response = []
-    } catch (error) {
-        console.log(error)
-    }
-}
+const express = require('express')
+const app = express()
+const port = 3002
+const fs = require("fs");
+
+app.get('/', (req, res) => {
+    res.send({ message: "all good" })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
